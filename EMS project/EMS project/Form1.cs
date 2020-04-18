8357e846c817b02f59841fe840bcce00dc3c0433
SunanRashidAIUB <sunanrashid@gmail.com>
2020-04-19 02:13:21 +0600

    Cerating a log in form
    I have creted a login form and added a table in database(Table: LoginForm) everypne just check.

--- EMS project/EMS project/Form1.cs
+++ EMS project/EMS project/Form1.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace EMS_project
 {
-    public partial class Form1 : Form
+    public partial class LoginForm : Form
     {
-        public Form1()
+        public LoginForm()
         {
             InitializeComponent();
         }
--- EMS project/EMS project/Program.cs
+++ EMS project/EMS project/Program.cs
@@ -16,7 +16,7 @@ namespace EMS_project
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new LoginForm());
         }
     }
 }
